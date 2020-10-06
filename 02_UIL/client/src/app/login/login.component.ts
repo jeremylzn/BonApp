be8a3698cab66bbc8b04f69c633b8414523d8129
jeremylzn <lznjeremy@gmail.com>
2020-10-06 12:55:54 +0300

    design of the header/navbar fixed

--- 02_UIL/client/src/app/login/login.component.ts
+++ 02_UIL/client/src/app/login/login.component.ts
@@ -4,21 +4,30 @@ import { Router } from '@angular/router';
 
 import { AuthService } from '../shared/services/auth.service';
 import { User } from '../shared/models/user.model';
+import { NavbarService } from '../shared/services/navbar.service';
+
+import { fade } from '../animations';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  animations: [
+    fade
+  ]
 })
 export class LoginComponent implements OnInit {
   public user: User;
   public signupForm: FormGroup;
   isLoading: boolean = false;
 
-  constructor(private AuthService: AuthService, private router: Router) {
+  constructor(private AuthService: AuthService, private router: Router, private navbarService:NavbarService) {
   }
 
   ngOnInit(): void {
+    this.navbarService.changeHeaderTitle('Login') // Send the title to NavbarService
+
     this.signupForm = new FormGroup({
       password: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
