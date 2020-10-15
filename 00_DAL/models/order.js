770790340c2a1d055084bb8b005dd1ea64d68cee
ofir5551 <ofirbenyamin3@gmail.com>
2020-10-16 01:34:00 +0300

    added itemSubSchema and customerSubSchema in orderSchema

--- 00_DAL/models/order.js
+++ 00_DAL/models/order.js
@@ -1,76 +1,99 @@
-const mongoose = require('mongoose')
-const User = require('./user')
-const moment = require('moment')
+const mongoose = require("mongoose");
+const moment = require("moment");
 
-const itemSchema = new mongoose.Schema({
+// Item Sub-Schema that will populate orderSchema 'items' field.
+const itemSubSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     quantity: {
-        type: Number,
-        required: true,
-        min: 1
-    }
+      type: Number,
+      required: true,
+      min: 1,
+    },
+  },
+  { _id: false }
+);
+// User Sub-Schema that will populate orderSchema 'customerDetails' field.
+const customerSubSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String
+    },
+  },
+  { _id: false }
+);
 
-});
 const orderSchema = new mongoose.Schema({
-    orderID: {
-        type: Number
-    },
-    customerID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    items: {    // Add validation to quantity and price so it can't be negative (add Item sub-schema?)
-        type: [itemSchema],
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    totalPrice: {
-        type: Number,
-        default: 0
-    },
-    date: {
-        type: String
-    },
-    time: {
-        type: String
-    }
-})
+  orderID: {
+    type: Number,
+  },
+  customerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    // required: true,
+  },
+  customerDetails: {
+    type: customerSubSchema,
+    required: true,
+  },
+  items: {
+    type: [itemSubSchema],
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  totalPrice: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+});
 
 // This function runs before saving the new order and does the following:
 // 1. Calculate the total price of the order into 'totalPrice' property
 // 2. Get the date and time of the order into 'date' and 'time' properties
 // .........................................................................
 orderSchema.pre("save", async function (next) {
-    const order = this
+  const order = this;
 
-    order.items.forEach(item => {
-        order.totalPrice += item.price * item.quantity
-    })
+  order.items.forEach((item) => {
+    order.totalPrice += item.price * item.quantity;
+  });
 
-    const today = new moment()
-    let currentDate = today.format('DD-MM-YYYY')
-    let currentTime = today.format('HH:mm:ss')
+  const today = new moment();
+  let currentDate = today.format("DD-MM-YYYY");
+  let currentTime = today.format("HH:mm:ss");
 
-    order.orderID = Math.floor(Math.random()*999+1)
-    order.date = currentDate
-    order.time = currentTime
+  order.orderID = Math.floor(Math.random() * 999 + 1);
+  order.date = currentDate;
+  order.time = currentTime;
 
-    next()
-})
+  next();
+});
 
+const Order = mongoose.model("Order", orderSchema);
 
-const Order = mongoose.model('Order', orderSchema)
-
-module.exports = Order
+module.exports = Order;
--- 00_DAL/models/user.js
+++ 00_DAL/models/user.js
@@ -1,98 +1,105 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true, // set field as required
+    trim: true, // removes spaces from beginning and end of a string
+    lowercase: true, // use .toLowerCase() on the string
+    validate: (value) => {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid!");
+      }
     },
-    email: {
-        type: String,
-        unique: true,
-        required: true, // set field as required
-        trim: true,     // removes spaces from beginning and end of a string
-        lowercase: true, // use .toLowerCase() on the string
-        validate: (value) => {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid!')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true
-    },
-    token: {
-        type: String
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    notifications: {
-        type: Array
-    }
-})
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  notifications: {
+    type: Array,
+  },
+});
 
 // Create a virtual property (a link between task.owner -> user._id)
 // in order to set a relationship between the two.
-userSchema.virtual('orders', {
-    ref: 'Order',
-    localField: '_id',
-    foreignField: 'customerID'
-})
+userSchema.virtual("orders", {
+  ref: "Order",
+  localField: "_id",
+  foreignField: "customerID",
+});
 
-// This function generates a jwt and stores 
+// This function generates a jwt and stores
 // it in the database for the current user
 userSchema.methods.generateAuthToken = async function () {
-    const user = this
+  const user = this;
 
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisisasecrettoken')
-    user.token = token
+  const token = jwt.sign({ _id: user._id.toString() }, "thisisasecrettoken");
+  user.token = token;
 
-    await user.save()
+  await user.save();
 
-    return token
-}
+  return token;
+};
 
 // This function gets email and password, and verifies them
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email: email })
+  const user = await User.findOne({ email: email });
 
-    if (!user) {
-        throw new Error('User not found')
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password)
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error('Password does not match')
-    }
+  if (!isMatch) {
+    throw new Error("Password does not match");
+  }
 
-    return user
-}
+  return user;
+};
 
 // Middleware for hashing the password using bcrypt algorithm
 // This runs just before saving the document
-userSchema.pre('save', async function (next) {
-    const user = this
+userSchema.pre("save", async function (next) {
+  const user = this;
 
-    // Check if password was modified to prevent double hashing
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
+  // Check if password was modified to prevent double hashing
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    if (user.notifications.length > 5) {
-        user.notifications.pop()
-    }
+  if (user.notifications.length > 5) {
+    user.notifications.pop();
+  }
 
-    next()
-})
+  next();
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
